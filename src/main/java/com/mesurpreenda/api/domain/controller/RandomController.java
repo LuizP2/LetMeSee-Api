37faad407143a4faa25990921a6a137f1495ae67
LuizP2 <luizpaulo2018.lp@gmail.com>
2025-05-24 14:10:13 -0300

    Renamed SearchController.java to RandomController.java

--- src/main/java/com/mesurpreenda/api/domain/controller/RandomController.java
+++ src/main/java/com/mesurpreenda/api/domain/controller/RandomController.java
@@ -9,16 +9,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
-public class SearchController {
+public class RandomController {
     @Autowired
     private MovieDAO movieDAO;
     @Autowired
